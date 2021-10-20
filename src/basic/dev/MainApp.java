3b598b806ee5465857b091a9fbb296b45b9aab20
Realoanh0054 <oanh1045@gmail.com>
2021-10-20 15:43:29 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,46 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Thông tin một sinh viên
+		 */
+		String tsv = "HO THI OANH";
+		System.out.println("Ten sinh vien: " + tsv);
+		String msv = "2160170";
+		System.out.println("Mã sinh viên: " + msv);
+		String cmnd = "192058913";
+		System.out.println("Chứng minh nhân dân: " +cmnd);
+		String tenlop = "21cdth41";
+		System.out.println("Tên lớp: " +tenlop);
+		String tentruong = "hueic";
+		System.out.println("Tên trường: " +tentruong);
+		float  dtb = 8.5f;
+		System.out.println("Điểm trung bình: " +dtb);
+		/**
+		 * Thông tin công nhân
+		 */
+		String tcn = "Oanh";
+		System.out.println("Tên công nhân: " +tcn);
+		String dc = "Thua Thien Hue";
+		System.out.println("Địa chỉ: " +dc);
+		String sdt = "0703100567";
+		System.out.println("Số điện thoại: " +sdt);
+		double  hsl = 6.5f;
+		System.out.println("Hệ số lương: " +hsl);
+		/**
+		 * Thông tin một chiếc xe car
+		 */
+		String tx = "Ford Territory";
+		System.out.println("Tên xe: " +tx);
+		String hx = "Ford";
+		System.out.println("Hãng xe: " +hx);
+		String gia = "699 triệu";
+		System.out.println("Giá: " +gia);
+		String ms = "white";
+		System.out.println("Màu sắc: " +ms);
+	}
+
+}
